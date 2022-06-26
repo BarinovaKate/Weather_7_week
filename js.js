601493aa0eee01ee126f7d4e53bc51e78e09a4c3
Kate Barinova <elena19062017@yahoo.com>
2022-06-26 17:18:02 +0300

    add api

---
+++ js.js
@@ -0,0 +1,40 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day}  ${hours}:${minutes}`;
+}
+
+
+function displayTemperature(responce) {
+   
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    temperatureElement.innerHTML = Math.round(responce.data.main.temp);
+    cityElement.innerHTML = responce.data.name;
+    descriptionElement.innerHTML = responce.data.weather[0].description;
+    humidityElement.innerHTML = responce.data.main.humidity;
+    windElement.innerHTML = Math.round(responce.data.wind.speed);
+    dateElement.innerHTML = formatDate(responce.data.dt *1000);
+}
+
+
+
+let apiKey = "360f33424340a0f95a8c619e705e8605";
+let cityName = "Glasgo";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
