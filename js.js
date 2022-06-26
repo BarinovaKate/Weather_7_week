41e13ef83df9297a8887734b63d82f6183d0dab3
Kate Barinova <elena19062017@yahoo.com>
2022-06-26 17:47:52 +0300

    change icon and date

--- js.js
+++ js.js
@@ -22,18 +22,21 @@ function displayTemperature(responce) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(responce.data.main.temp);
     cityElement.innerHTML = responce.data.name;
     descriptionElement.innerHTML = responce.data.weather[0].description;
     humidityElement.innerHTML = responce.data.main.humidity;
     windElement.innerHTML = Math.round(responce.data.wind.speed);
-    dateElement.innerHTML = formatDate(responce.data.dt *1000);
+    dateElement.innerHTML = formatDate(responce.data.dt * 1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", responce.data.weather[0].description);
 }
 
 
 
 let apiKey = "360f33424340a0f95a8c619e705e8605";
-let cityName = "Glasgo";
+let cityName = "Kherson";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
