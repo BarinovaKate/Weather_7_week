9663be7227c0cce3a25ed9b3c77e7d455143f1d7
Kate Barinova <elena19062017@yahoo.com>
2022-06-26 22:52:43 +0300

    added current position

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+// format date
 function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
@@ -13,7 +14,7 @@ function formatDate(timestamp) {
     return `${day}  ${hours}:${minutes}`;
 }
 
-
+//show weather element
 function displayTemperature(responce) {
    
     let temperatureElement = document.querySelector("#temperature");
@@ -34,18 +35,19 @@ function displayTemperature(responce) {
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", responce.data.weather[0].description);
 }
+//search city input
 function search(city) {
     let apiKey = "360f33424340a0f95a8c619e705e8605";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
-
+//submit "search"
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input")
     search(cityInputElement.value);
 }
-
+//converter C-F
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
     celsiusLink.classList.remove("active");
@@ -54,6 +56,7 @@ function displayFahrenheitTemperature(event) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+//converter F-C
 function displayCelsiusTemperature(event) {
     event.preventDefault(); 
     celsiusLink.classList.add("active");
@@ -63,17 +66,50 @@ function displayCelsiusTemperature(event) {
 }
 let celsiusTemperature = null;
 
-
+//submit form
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 
-
+//click F
 let fahrenheitLink = document.querySelector("#fahrenheit-link")
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
+//click C
 let celsiusLink = document.querySelector("#celsius-link")
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+//current position
+function currentCity(event) {
+  event.preventDefault();
+  
+  function myPosition(position) {
+  let apiKey = "360f33424340a0f95a8c619e705e8605";
+  let apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude; 
+  axios.get(`${apiUrl}&lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`).then(currentWeather);
+}
+  navigator.geolocation.getCurrentPosition(myPosition); 
+  function currentWeather(responce) {
+    document.querySelector("#city").innerHTML = `${responce.data.name}`;
+    document.querySelector("#temperature").innerHTML = `${Math.round(responce.data.main.temp)}`;
+    document.querySelector("#wind").innerHTML = `${Math.round(responce.data.wind.speed)}`;
+    document.querySelector("#humidity").innerHTML = `${responce.data.main.humidity}`;
+    document.querySelector("#description").innerHTML = `${responce.data.weather[0].description}`;
+    document.querySelector("#icon").setAttribute("src", `https://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`);
+    document.querySelector("#icon").setAttribute("alt", responce.data.weather[0].description);
+  }
+}
+document.querySelector("#current").addEventListener("click", currentCity);
+
+
+
+
+
+
+
+
 
 
 search("Kherson");
